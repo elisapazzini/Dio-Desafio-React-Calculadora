b51b21549feb8dfbfdd6132e96d12468920def9a
Elisa Pazzini <105680356+elisapazzini@users.noreply.github.com>
2022-12-10 16:16:21 -0300

    Add função Equal e Subtrair

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import { Container, Content, Row } from "./styles";
 export default function App() {
   const [currentNumber, setCurrentNumber] = React.useState('0')
   const [firstNumber, setFirstNumber] = React.useState('0')
+  const [operation, setOperation] = React.useState('0')
 
 
   const handleOnClear = () => {
     setCurrentNumber('0')
     setFirstNumber('0')
+    setOperation('')
   }
 
   const handleAddNumber = (number) => {
@@ -21,11 +23,49 @@ export default function App() {
   const handleSumNumbers = () => {
     if (firstNumber === '0') {
       setFirstNumber(String(currentNumber));
-      setCurrentNumber('0')
+      setCurrentNumber('') //setCurrentNumber('0')
+      setOperation('+')
     } else {
       const sum = Number(firstNumber) + Number(currentNumber)
       setCurrentNumber(String(sum))
+      setOperation('')
       
+    }
+  }
+
+  const handleSubNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('') //setCurrentNumber('0')
+      setOperation('-')
+    } else {
+      const sub = Number(firstNumber) - Number(currentNumber)
+      setCurrentNumber(String(sub))
+      setOperation('')
+      
+    }
+  }
+
+ //funcao multiplicar
+ //funcao dividir
+
+  const handleEquals = () => {
+    if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
+      switch (operation) {
+        case '+':
+          handleSumNumbers()
+          break;
+
+        case '-':
+          handleSubNumbers()
+          break
+      
+        default:
+          break;
+      }
+      
+      // setFirstNumber(String(currentNumber))
+      // setCurrentNumber('0')
     }
   }
 
@@ -51,7 +91,7 @@ export default function App() {
             <Button label={6} onClick={() => handleAddNumber('6')}></Button>
             <Button label={5} onClick={() => handleAddNumber('5')}></Button>
             <Button label={4} onClick={() => handleAddNumber('4')}></Button>
-            <Button label={"-"} onClick={() => handleAddNumber('')}></Button>
+            <Button label={"-"} onClick={handleSubNumbers}></Button>
           </Row>
           <Row>
             <Button label={3} onClick={() => handleAddNumber('3')}></Button>
@@ -63,7 +103,7 @@ export default function App() {
             <Button label={0} onClick={() => handleAddNumber('0')}></Button>
             <Button label={","} onClick={() => handleAddNumber(',')}></Button>
             <Button label={"<"} onClick={() => handleAddNumber('')}></Button>
-            <Button label={"="} onClick={() => handleAddNumber('')}></Button>
+            <Button label={"="} onClick={handleEquals}></Button>
           </Row>
         </Content>
       </Container>
